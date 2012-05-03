6e41076ff9e7082530beacd3556ec91c4a6c63ea
Jack Moxon <jsmoxon@gmail.com>
2012-05-02 22:54:47 -0700

    the chrome extension works except for custom listener, now to build that and the site

--- blockr.js
+++ blockr.js
@@ -1,36 +1,32 @@
-var somejson = '{"name":"jack","age":36}';
-var bla = JSON.parse(somejson);
-
-var test = "The law of caus";
-var flag =jQuery.getJSON("http://127.0.0.1:8000/json_test", function(jsonla) {
-    })
-
-    console.log(flag);
+var live_url = "http://jackmoxon.com/json_test";
+var flag = [];
+function check_json_flag() {
+    $.ajax({
+	url: live_url,
+	    async: false,
+	    dataType: 'json',
+	    success: function(json) {
+	    flag = json.name;
+	}
+    });
+return flag
+};
 
 //this works, but only if you reload the extension each time...so perhaps we have to have this at a lower level
-jQuery.getJSON("http://127.0.0.1:8000/json_test", function(jsonla) {
-	chrome.webRequest.onBeforeRequest.addListener(
+chrome.webRequest.onBeforeRequest.addListener(
 					      function(foo) {
-						  console.log("foo bar: "+foo.url);
-								  if (jsonla.name == "jack")
-						       {
-							   alert(jsonla.name)
-						      return {redirectUrl: "http://clothesliner.com"}
-						      alert("lasdlfk")
-						  }						      
+						  if (check_json_flag() == "okay") {
+							  }						      
 						  else {
-						      alert("the flag is not jack!")
 						      return {redirectUrl: "http://essayr.com"};
 						  };
 					      }
 						  ,
 					      {
 						  urls: [
-							 "http://*.roommater.us/*",
-							 "http://*.jackmoxon.com/*",
+							"<all_urls>"
 							 ],
 						      types: ["main_frame"]
 						      },
 					      //extrainfospec                                                                           
 					      ["blocking"]);
-    })
